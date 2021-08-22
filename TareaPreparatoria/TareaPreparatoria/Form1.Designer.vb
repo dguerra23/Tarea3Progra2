21128cbed9ef410a3acec8ad757723a0176a9aee
dguerra23 <dguerra2300@gmail.com>
2021-08-21 22:59:53 -0600

    Entrega tarea preparatoria

---
+++ TareaPreparatoria/TareaPreparatoria/Form1.Designer.vb
@@ -0,0 +1,424 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.HiloTxt = New System.Windows.Forms.TextBox()
+        Me.SedaTxt = New System.Windows.Forms.TextBox()
+        Me.AlgodonText = New System.Windows.Forms.TextBox()
+        Me.LinoText = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.HiloBtn = New System.Windows.Forms.CheckBox()
+        Me.SedaBtn = New System.Windows.Forms.CheckBox()
+        Me.AlgodonBtn = New System.Windows.Forms.CheckBox()
+        Me.LinoBtn = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.KingBtn = New System.Windows.Forms.RadioButton()
+        Me.QueenBtn = New System.Windows.Forms.RadioButton()
+        Me.MatrimonialBtn = New System.Windows.Forms.RadioButton()
+        Me.ImperialBtn = New System.Windows.Forms.RadioButton()
+        Me.CalcularBtn = New System.Windows.Forms.Button()
+        Me.LimpiarBtn = New System.Windows.Forms.Button()
+        Me.SalirBtn = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.HiloTxt2 = New System.Windows.Forms.TextBox()
+        Me.SedaTxt2 = New System.Windows.Forms.TextBox()
+        Me.AlgodonTxt2 = New System.Windows.Forms.TextBox()
+        Me.LinoTxt2 = New System.Windows.Forms.TextBox()
+        Me.PrecioVentaTxt = New System.Windows.Forms.TextBox()
+        Me.PrecioCostoTxt = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.GroupBox1.Controls.Add(Me.HiloTxt)
+        Me.GroupBox1.Controls.Add(Me.SedaTxt)
+        Me.GroupBox1.Controls.Add(Me.AlgodonText)
+        Me.GroupBox1.Controls.Add(Me.LinoText)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.HiloBtn)
+        Me.GroupBox1.Controls.Add(Me.SedaBtn)
+        Me.GroupBox1.Controls.Add(Me.AlgodonBtn)
+        Me.GroupBox1.Controls.Add(Me.LinoBtn)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.KingBtn)
+        Me.GroupBox1.Controls.Add(Me.QueenBtn)
+        Me.GroupBox1.Controls.Add(Me.MatrimonialBtn)
+        Me.GroupBox1.Controls.Add(Me.ImperialBtn)
+        Me.GroupBox1.Location = New System.Drawing.Point(29, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(366, 324)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Seleccionar"
+        '
+        'HiloTxt
+        '
+        Me.HiloTxt.Enabled = False
+        Me.HiloTxt.Location = New System.Drawing.Point(206, 282)
+        Me.HiloTxt.Name = "HiloTxt"
+        Me.HiloTxt.Size = New System.Drawing.Size(100, 23)
+        Me.HiloTxt.TabIndex = 14
+        '
+        'SedaTxt
+        '
+        Me.SedaTxt.Enabled = False
+        Me.SedaTxt.Location = New System.Drawing.Point(206, 252)
+        Me.SedaTxt.Name = "SedaTxt"
+        Me.SedaTxt.Size = New System.Drawing.Size(100, 23)
+        Me.SedaTxt.TabIndex = 13
+        '
+        'AlgodonText
+        '
+        Me.AlgodonText.Enabled = False
+        Me.AlgodonText.Location = New System.Drawing.Point(206, 222)
+        Me.AlgodonText.Name = "AlgodonText"
+        Me.AlgodonText.Size = New System.Drawing.Size(100, 23)
+        Me.AlgodonText.TabIndex = 12
+        '
+        'LinoText
+        '
+        Me.LinoText.Enabled = False
+        Me.LinoText.Location = New System.Drawing.Point(206, 192)
+        Me.LinoText.Name = "LinoText"
+        Me.LinoText.Size = New System.Drawing.Size(100, 23)
+        Me.LinoText.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(206, 168)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 15)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Cantidad de material"
+        '
+        'HiloBtn
+        '
+        Me.HiloBtn.AutoSize = True
+        Me.HiloBtn.Location = New System.Drawing.Point(19, 284)
+        Me.HiloBtn.Name = "HiloBtn"
+        Me.HiloBtn.Size = New System.Drawing.Size(84, 19)
+        Me.HiloBtn.TabIndex = 9
+        Me.HiloBtn.Text = "Hilo Crudo"
+        Me.HiloBtn.UseVisualStyleBackColor = True
+        '
+        'SedaBtn
+        '
+        Me.SedaBtn.AutoSize = True
+        Me.SedaBtn.Location = New System.Drawing.Point(19, 254)
+        Me.SedaBtn.Name = "SedaBtn"
+        Me.SedaBtn.Size = New System.Drawing.Size(51, 19)
+        Me.SedaBtn.TabIndex = 8
+        Me.SedaBtn.Text = "Seda"
+        Me.SedaBtn.UseVisualStyleBackColor = True
+        '
+        'AlgodonBtn
+        '
+        Me.AlgodonBtn.AutoSize = True
+        Me.AlgodonBtn.Location = New System.Drawing.Point(19, 224)
+        Me.AlgodonBtn.Name = "AlgodonBtn"
+        Me.AlgodonBtn.Size = New System.Drawing.Size(72, 19)
+        Me.AlgodonBtn.TabIndex = 7
+        Me.AlgodonBtn.Text = "Algodon"
+        Me.AlgodonBtn.UseVisualStyleBackColor = True
+        '
+        'LinoBtn
+        '
+        Me.LinoBtn.AutoSize = True
+        Me.LinoBtn.Location = New System.Drawing.Point(19, 194)
+        Me.LinoBtn.Name = "LinoBtn"
+        Me.LinoBtn.Size = New System.Drawing.Size(49, 19)
+        Me.LinoBtn.TabIndex = 6
+        Me.LinoBtn.Text = "Lino"
+        Me.LinoBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Tipo de material"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Tipo de cubrecama"
+        '
+        'KingBtn
+        '
+        Me.KingBtn.AutoSize = True
+        Me.KingBtn.Location = New System.Drawing.Point(19, 128)
+        Me.KingBtn.Name = "KingBtn"
+        Me.KingBtn.Size = New System.Drawing.Size(49, 19)
+        Me.KingBtn.TabIndex = 3
+        Me.KingBtn.TabStop = True
+        Me.KingBtn.Text = "King"
+        Me.KingBtn.UseVisualStyleBackColor = True
+        '
+        'QueenBtn
+        '
+        Me.QueenBtn.AutoSize = True
+        Me.QueenBtn.Location = New System.Drawing.Point(19, 102)
+        Me.QueenBtn.Name = "QueenBtn"
+        Me.QueenBtn.Size = New System.Drawing.Size(60, 19)
+        Me.QueenBtn.TabIndex = 2
+        Me.QueenBtn.TabStop = True
+        Me.QueenBtn.Text = "Queen"
+        Me.QueenBtn.UseVisualStyleBackColor = True
+        '
+        'MatrimonialBtn
+        '
+        Me.MatrimonialBtn.AutoSize = True
+        Me.MatrimonialBtn.Location = New System.Drawing.Point(19, 76)
+        Me.MatrimonialBtn.Name = "MatrimonialBtn"
+        Me.MatrimonialBtn.Size = New System.Drawing.Size(90, 19)
+        Me.MatrimonialBtn.TabIndex = 1
+        Me.MatrimonialBtn.TabStop = True
+        Me.MatrimonialBtn.Text = "Matrimonial"
+        Me.MatrimonialBtn.UseVisualStyleBackColor = True
+        '
+        'ImperialBtn
+        '
+        Me.ImperialBtn.AutoSize = True
+        Me.ImperialBtn.Location = New System.Drawing.Point(19, 50)
+        Me.ImperialBtn.Name = "ImperialBtn"
+        Me.ImperialBtn.Size = New System.Drawing.Size(68, 19)
+        Me.ImperialBtn.TabIndex = 0
+        Me.ImperialBtn.TabStop = True
+        Me.ImperialBtn.Text = "Imperial"
+        Me.ImperialBtn.UseVisualStyleBackColor = True
+        '
+        'CalcularBtn
+        '
+        Me.CalcularBtn.Location = New System.Drawing.Point(421, 254)
+        Me.CalcularBtn.Name = "CalcularBtn"
+        Me.CalcularBtn.Size = New System.Drawing.Size(75, 23)
+        Me.CalcularBtn.TabIndex = 1
+        Me.CalcularBtn.Text = "Calcular"
+        Me.CalcularBtn.UseVisualStyleBackColor = True
+        '
+        'LimpiarBtn
+        '
+        Me.LimpiarBtn.Location = New System.Drawing.Point(502, 254)
+        Me.LimpiarBtn.Name = "LimpiarBtn"
+        Me.LimpiarBtn.Size = New System.Drawing.Size(75, 23)
+        Me.LimpiarBtn.TabIndex = 2
+        Me.LimpiarBtn.Text = "Limpiar"
+        Me.LimpiarBtn.UseVisualStyleBackColor = True
+        '
+        'SalirBtn
+        '
+        Me.SalirBtn.Location = New System.Drawing.Point(584, 254)
+        Me.SalirBtn.Name = "SalirBtn"
+        Me.SalirBtn.Size = New System.Drawing.Size(75, 23)
+        Me.SalirBtn.TabIndex = 3
+        Me.SalirBtn.Text = "Salir"
+        Me.SalirBtn.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(450, 28)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 15)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Precio de costo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(450, 58)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 15)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Precio Venta"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(450, 79)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(111, 15)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Costo de materiales"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(450, 103)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(30, 15)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Lino"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(450, 133)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 15)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Algodon"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(450, 163)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(32, 15)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Seda"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(450, 193)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(63, 15)
+        Me.Label10.TabIndex = 10
+        Me.Label10.Text = "Hilo crudo"
+        '
+        'HiloTxt2
+        '
+        Me.HiloTxt2.Location = New System.Drawing.Point(535, 190)
+        Me.HiloTxt2.Name = "HiloTxt2"
+        Me.HiloTxt2.Size = New System.Drawing.Size(100, 23)
+        Me.HiloTxt2.TabIndex = 18
+        '
+        'SedaTxt2
+        '
+        Me.SedaTxt2.Location = New System.Drawing.Point(535, 160)
+        Me.SedaTxt2.Name = "SedaTxt2"
+        Me.SedaTxt2.Size = New System.Drawing.Size(100, 23)
+        Me.SedaTxt2.TabIndex = 17
+        '
+        'AlgodonTxt2
+        '
+        Me.AlgodonTxt2.Location = New System.Drawing.Point(535, 130)
+        Me.AlgodonTxt2.Name = "AlgodonTxt2"
+        Me.AlgodonTxt2.Size = New System.Drawing.Size(100, 23)
+        Me.AlgodonTxt2.TabIndex = 16
+        '
+        'LinoTxt2
+        '
+        Me.LinoTxt2.Location = New System.Drawing.Point(535, 100)
+        Me.LinoTxt2.Name = "LinoTxt2"
+        Me.LinoTxt2.Size = New System.Drawing.Size(100, 23)
+        Me.LinoTxt2.TabIndex = 15
+        '
+        'PrecioVentaTxt
+        '
+        Me.PrecioVentaTxt.Location = New System.Drawing.Point(544, 55)
+        Me.PrecioVentaTxt.Name = "PrecioVentaTxt"
+        Me.PrecioVentaTxt.Size = New System.Drawing.Size(100, 23)
+        Me.PrecioVentaTxt.TabIndex = 16
+        '
+        'PrecioCostoTxt
+        '
+        Me.PrecioCostoTxt.Location = New System.Drawing.Point(544, 25)
+        Me.PrecioCostoTxt.Name = "PrecioCostoTxt"
+        Me.PrecioCostoTxt.Size = New System.Drawing.Size(100, 23)
+        Me.PrecioCostoTxt.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGreen
+        Me.ClientSize = New System.Drawing.Size(673, 348)
+        Me.Controls.Add(Me.PrecioVentaTxt)
+        Me.Controls.Add(Me.HiloTxt2)
+        Me.Controls.Add(Me.PrecioCostoTxt)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.SedaTxt2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.AlgodonTxt2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.LinoTxt2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.SalirBtn)
+        Me.Controls.Add(Me.LimpiarBtn)
+        Me.Controls.Add(Me.CalcularBtn)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents KingBtn As RadioButton
+    Friend WithEvents QueenBtn As RadioButton
+    Friend WithEvents MatrimonialBtn As RadioButton
+    Friend WithEvents ImperialBtn As RadioButton
+    Friend WithEvents HiloTxt As TextBox
+    Friend WithEvents SedaTxt As TextBox
+    Friend WithEvents AlgodonText As TextBox
+    Friend WithEvents LinoText As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents HiloBtn As CheckBox
+    Friend WithEvents SedaBtn As CheckBox
+    Friend WithEvents AlgodonBtn As CheckBox
+    Friend WithEvents LinoBtn As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CalcularBtn As Button
+    Friend WithEvents LimpiarBtn As Button
+    Friend WithEvents SalirBtn As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents HiloTxt2 As TextBox
+    Friend WithEvents SedaTxt2 As TextBox
+    Friend WithEvents AlgodonTxt2 As TextBox
+    Friend WithEvents LinoTxt2 As TextBox
+    Friend WithEvents PrecioVentaTxt As TextBox
+    Friend WithEvents PrecioCostoTxt As TextBox
+End Class
---
+++ TareaPreparatoria/TareaPreparatoria/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles KingBtn.CheckedChanged
+
+    End Sub
+
+    Private Sub CalcularBtn_Click(sender As Object, e As EventArgs) Handles CalcularBtn.Click
+        Dim OMImperial As Double
+        Dim OMMatrimonial As Double
+        Dim OMQueen As Double
+        Dim OMKing As Double
+        Dim Lino As Double
+        Dim Algodon As Double
+        Dim Seda As Double
+        Dim Hilo As Double
+        Dim PrecioCosto As Double
+        Dim PrecioVenta As Double
+
+
+    End Sub
+
+    Private Sub LinoBtn_CheckedChanged(sender As Object, e As EventArgs) Handles LinoBtn.CheckedChanged
+        If LinoBtn.Checked Then
+            LinoText.Enabled = True
+        End If
+    End Sub
+
+    Private Sub AlgodonBtn_CheckedChanged(sender As Object, e As EventArgs) Handles AlgodonBtn.CheckedChanged
+        If AlgodonBtn.Checked Then
+            AlgodonText.Enabled = True
+        End If
+    End Sub
+
+    Private Sub SedaBtn_CheckedChanged(sender As Object, e As EventArgs) Handles SedaBtn.CheckedChanged
+        If SedaBtn.Checked Then
+            SedaTxt.Enabled = True
+        End If
+    End Sub
+
+    Private Sub HiloBtn_CheckedChanged(sender As Object, e As EventArgs) Handles HiloBtn.CheckedChanged
+        If HiloBtn.Checked Then
+            HiloTxt.Enabled = True
+        End If
+    End Sub
+End Class
